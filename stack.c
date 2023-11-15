85e1631dc3298ab089f49a224d78c6866df8a861
Mamabolo Katlego <rabotaikatlego@gmail.com>
2023-11-15 04:35:02 -0800

    stack.c

---
+++ stack.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * f_stack - It prints the top
+ * @head: The stack head
+ * @counter: line_num
+ * Return: There is no return
+ */
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
