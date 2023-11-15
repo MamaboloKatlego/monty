ea386f827a3098a9a3fcc18a350ee37969b50c73
Mamabolo Katlego <rabotaikatlego@gmail.com>
2023-11-15 05:30:15 -0800

    nop.c

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * f_nop - There is nothing
+ * @head: THe stack head
+ * @counter: line_num
+ * Return: There is no return
+ */
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
